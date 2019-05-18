d1288577c5eeea17709a060acde0ce84a639a481
SaliouMbalo <mballosaliou92@yahoo.com>
2019-05-18 21:28:32 +0000

    suppression

--- projet-python/moyenne.py
+++
@@ -1,8 +0,0 @@
-#coding:utf-8
-a=input("Veuillez entrer votre première note?")
-b=input("Veuillez entrer votre deuxième note?")
-a=float(a)
-b=float(b)
-s=(a+b)
-m=float(s/2)
-print("La moyenne de vos notes est égale à:",m)
--- projet-python/pair_impair.py
+++
@@ -1,6 +0,0 @@
-#coding:utf-8
-x=int(input("Veuillez entrer un nombre entier "))
-if x%2 == 0:
-    print("Nombre pair")
-else:
-    print("Nombre impair")
